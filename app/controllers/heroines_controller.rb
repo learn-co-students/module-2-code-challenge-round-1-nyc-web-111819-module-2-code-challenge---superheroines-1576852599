78c45c5666b3f603a078a480c3e830f3d6dd28ca
warren mcdonald <warrenjmac@mail.com>
2019-12-20 11:12:28 -0500

    warren

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new 
+    @heroine = Heroine.new
+  end
+
+  def create 
+    @heroine = Heroine.create(strong)
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private 
+
+  def strong
+      params.require(params).permit(:name, :super_name)
+   end
 end
