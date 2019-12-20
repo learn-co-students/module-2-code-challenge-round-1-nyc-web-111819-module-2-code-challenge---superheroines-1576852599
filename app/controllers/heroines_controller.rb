d1da34cf8e2699f43d43c22f22eb5d1b969649f3
kevinkim1030 <kevin.kim1030@gmail.com>
2019-12-20 11:12:05 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,38 @@
 class HeroinesController < ApplicationController
+
+  before_action :find_heroine, only: [:show]  
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    # @heroine = Heroine.find(params[:id])  
+    # @power = Power.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:messages] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
